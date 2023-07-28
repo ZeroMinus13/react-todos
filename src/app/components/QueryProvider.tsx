5df9cd83cf55cb88306d489f640064ca80ffc98d
ZeroMinus13 <zerominus13@gmail.com>
2023-07-28 13:26:13 +0500

    removing unused code

--- src/app/components/QueryProvider.tsx
+++ src/app/components/QueryProvider.tsx
@@ -3,17 +3,14 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { cache } from 'react';
 
-const queryClient = cache(
-  () =>
-    new QueryClient({
-      defaultOptions: {
-        queries: {
-          refetchOnWindowFocus: false,
-        },
-      },
-    })
-);
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 export default function QueryProvider({ children }: { children: React.ReactNode }) {
-  return <QueryClientProvider client={queryClient()}>{children}</QueryClientProvider>;
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
