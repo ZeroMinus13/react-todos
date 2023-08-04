37f372dadd1a937f20c7b5efb5be384da50e5ff1
ZeroMinus13 <zerominus13@gmail.com>
2023-08-04 08:27:26 +0500

    Error in netlify

---
+++ src/app/components/ErrorBoundry.tsx
@@ -0,0 +1,32 @@
+import { Component, ErrorInfo, ReactNode } from 'react';
+
+interface Props {
+  children?: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+export default class ErrorBoundary extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error('Uncaught error:', error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1 className='text-3xl text-center'>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,19 @@ import Loading from './loading';
 import { Suspense } from 'react';
 import { getData } from './api/actions';
 import Navbar from './components/Navbar';
-
+import ErrorBoundary from './components/ErrorBoundry';
 async function Home() {
   const users = await getData();
 
   return (
     <main className='flex min-h-screen flex-col items-center bg-slate-600 p-5'>
-      <Navbar />
-      <Form defaultShow={false} />
-      <Suspense fallback={<Loading />}>
-        <TodosList users={users} />
-      </Suspense>
+      <ErrorBoundary>
+        <Navbar />
+        <Form defaultShow={false} />
+        <Suspense fallback={<Loading />}>
+          <TodosList users={users} />
+        </Suspense>
+      </ErrorBoundary>
     </main>
   );
 }
